fca13af59a4d8855dd6ad7c3bfd5b47283b7568f
Valdiq <stasyshyn.vld@gmail.com>
2024-09-01 16:07:10 +0200

    init

---
+++ src/main/java/org/example/cleanup/CleanUpApplication.java
@@ -0,0 +1,13 @@
+package org.example.cleanup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cleanup/CleanUpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cleanup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
